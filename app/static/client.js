30a6cfa47e01ab4a1351cc3bd514a44c4b561e8a
Cedric Chee <cedric@invictusbyte.com>
2018-12-05 20:37:00 +0800

    Show top-3 results on UI

--- app/static/client.js
+++ app/static/client.js
@@ -24,7 +24,8 @@ function analyze() {
     xhr.onload = function(e) {
         if (this.readyState === 4) {
             var response = JSON.parse(e.target.responseText);
-            el('result-label').innerHTML = `Result = ${response['textResult']}`;
+            el('result-label').innerHTML = `<strong>Result:</strong><br />${response['textResult']}`;
+            el('top3-result-label').innerHTML = `<strong>Top-3 Result:</strong>${response['top_3_result']}`;
         }
         el('analyze-button').innerHTML = 'Analyze';
     }

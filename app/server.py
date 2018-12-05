5c9e57dfa641f1934c924bba3622365e7addaba8
Cedric Chee <cedric@invictusbyte.com>
2018-12-05 20:20:54 +0800

    Show top-3 predictions

--- app/server.py
+++ app/server.py
@@ -76,15 +76,27 @@ async def analyze(request):
     img_bytes = await (data["file"].read())
     img = open_image(BytesIO(img_bytes))
     predictions = []
+    top_losses = []
     for learn in learners:
         print("prediction:", learn.predict(img)[0])
-        predictions.append(learn.predict(img)[0])
+        pred_class, pred_idx, losses = learn.predict(img)
+        predictions.append(pred_class) # same as learn.predict(img)[0]
+        top_losses.append(losses)
+
     print("length of predictions list:", len(predictions))
+
+    top3_preds = []
     for c in range(len(predictions)):
         print("modelName:", modelDefs[c]["name"])
         print("type of predictions[c]:", type(predictions[c]))
         print("prediction:", predictions[c])
         print("textResult:", "modelName:" + modelDefs[c]["name"] + " - prediction:" + str(predictions[c]))
+
+        top_preds = sorted(zip(modelDefs[c]["classes"], map(float, top_losses[c])), key=lambda p: p[1], reverse=True)
+        top3 = top_preds[0:3]
+        top3_preds.append(top3)
+        print("top-3 preds:", str(top3))
+
     return JSONResponse(
         {
             "textResult": [
@@ -99,6 +111,10 @@ async def analyze(request):
                 {"modelName": modelDefs[c]["name"], "prediction": str(predictions[c])}
                 for c in range(len(predictions))
             ],
+            "top_3_result": [
+                {"modelName": modelDefs[c]["name"], "prediction": str(top3_preds[c])}
+                for c in range(len(top3_preds))
+            ]
         }
     )
 
